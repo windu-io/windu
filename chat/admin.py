8518d4f3a7f4d5a0fc0af054fe3f6aec19a3ba66
Vinicius Jarina <viniciusjarina@gmail.com>
2015-11-09 04:02:53 -0200

    Updated models. Added account model.

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from .models import Chat
+from .models import Message
+from .models import Contact
+from .models import Picture
+from .models import Account
+
+admin.site.register(Account)
+admin.site.register(Chat)
+admin.site.register(Contact)
+admin.site.register(Message)
+admin.site.register(Picture)
--- chat/models.py
+++ chat/models.py
@@ -1,48 +1,70 @@
 from django.db import  models
 
+class Account(models.Model):
+    id = models.CharField (max_length=64, primary_key=True)
+    password = models.CharField (max_length=64, null=False)
+    nick = models.CharField (max_length=256, null=True, blank=True)
+    def __str__(self):
+        return self.nick
+
 class Chat(models.Model):
     class Meta:
-        unique_together = (('account_id', 'entity_id'),)
-    account_id = models.CharField (max_length=64, db_index=True,null=False)
+        unique_together = (('account', 'entity_id'),)
+    account = models.ForeignKey (Account)
     entity_id  = models.CharField (max_length=64, db_index=True,null=False)
-    title  = models.CharField (max_length=256)
-    snippet  = models.CharField (max_length=64)
-    time = models.DateTimeField ()
+    title  = models.CharField (max_length=256,null=False)
+    snippet  = models.CharField (max_length=64, null=False)
+    time = models.DateTimeField (null=True,blank=True)
+    def __str__(self):
+        return  self.title
 
 class Message (models.Model):
     class Meta:
-        unique_together = (('account_id', 'message_id'),)
-    account_id = models.CharField (max_length=64, db_index=True,null=False)
+        unique_together = (('account', 'message_id'),)
+    account = models.ForeignKey (Account)
     message_id = models.CharField (max_length=64, db_index=True,null=False)
     entity_id = models.CharField (max_length=64, db_index=True,null=False)
-    send_type = models.CharField (max_length=1)
-    message_type = models.CharField (max_length=1)
-    time = models.DateTimeField ()
-    data = models.TextField ()
-    url = models.URLField ()
-    longitute = models.FloatField ()
-    latitude = models.FloatField ()
-    mimetype = models.CharField (max_length=64)
-    file_hash = models.CharField (max_length=128)
-    height = models.SmallIntegerField ()
-    size = models.SmallIntegerField ()
-    vcodec = models.CharField (max_length=12)
-    acodec = models.CharField (max_length=12)
-    duration = models.SmallIntegerField()
-    caption = models.CharField (max_length=256)
-    participant = models.CharField (max_length=64)
-    sent = models.DateTimeField ()
-    delivered = models.DateTimeField ()
-    read = models.DateTimeField ()
+    send_type = models.CharField (max_length=1, null=False)
+    message_type = models.CharField (max_length=1, null=False)
+    time = models.DateTimeField (null=False)
+    data = models.TextField (null=False)
+    url = models.URLField (null=True,blank=True)
+    longitute = models.FloatField (null=True,blank=True)
+    latitude = models.FloatField (null=True,blank=True)
+    mimetype = models.CharField (max_length=64, null=True,blank=True)
+    file_hash = models.CharField (max_length=128, null=True, blank=True)
+    height = models.SmallIntegerField (null=True, blank=True)
+    size = models.SmallIntegerField (null=True, blank=True)
+    vcodec = models.CharField (max_length=12, null=True, blank=True)
+    acodec = models.CharField (max_length=12, null=True, blank=True)
+    duration = models.SmallIntegerField(null=True, blank=True)
+    caption = models.CharField (max_length=256, null=True, blank=True)
+    participant = models.CharField (max_length=64, null=True, blank=True)
+    sent = models.DateTimeField (null=True, blank=True)
+    delivered = models.DateTimeField (null=True, blank=True)
+    read = models.DateTimeField (null=True, blank=True)
+    def __str__(self):
+        return self.message_id
 
 class Contact (models.Model):
     class Meta:
-        unique_together = (('account_id', 'entity_id'),)
-    account_id = models.CharField (max_length=64, db_index=True,null=False)
+        unique_together = (('account', 'contact_id'),)
+    account = models.ForeignKey (Account)
+    contact_id  = models.CharField (max_length=64, db_index=True,null=False)
+    title  = models.CharField (max_length=256, null=True, blank=True)
+    status = models.CharField (max_length=256, null=True, blank=True)
+    last_seen = models.DateTimeField (null=True, blank=True)
+    def __str__(self):
+        return self.title
+
+class Picture (models.Model):
+    class Meta:
+        unique_together = (('account', 'entity_id'),)
+    account = models.ForeignKey (Account)
     entity_id  = models.CharField (max_length=64, db_index=True,null=False)
-    title  = models.CharField (max_length=256)
-    status = models.CharField (max_length=256)
-    last_seen = models.DateTimeField ()
-    picture = models.URLField ()
+    updated = models.DateTimeField (null=False,auto_now=True)
+    picture = models.URLField (null=False)
+    def __str__(self):
+        return self.picture
 # Create your models here.
 
