058ae459b9b0b4db6ecbd6f72f587834714430cc
Vinicius Jarina <viniciusjarina@gmail.com>
2015-12-15 00:06:25 -0200

    Initial implementation views.

--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from django.shortcuts import render
+from django.http import Http404
 
-def index(request):
-    return HttpResponse("Chat index.")
+# Create your views here.
+from .controller import get_controller
+
+def chats(request):
+
+    chat_list = get_controller().chats()
+
+    return render(request, 'chats.html',{
+        'chat_list': chat_list
+    })
+
+def messages(request, entity_id):
+
+    message_list = get_controller().messages(entity_id)
+
+    if (message_list == None):
+        raise Http404 ("Message not found")
+
+    return  render(request,'messages.html',{
+        'message_list': message_list
+    })
